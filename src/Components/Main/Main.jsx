878fae40978c4a3f82fae6b031983fdedd8576a0
jurgilijus <jurgissubacius@gmail.com>
2023-02-12 17:22:33 +0200

    main WIP

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,7 +4,18 @@ import React from "react";
 import "./Main.css";
 
 function Main() {
-  return <section className="Main-conteiner">Main</section>;
+  return (
+    <section className="Main-conteiner">
+      <article className="main1-photo">
+        <a href="/" className="main1-photo-text-content">
+          <h1 className="main1-photo-text">
+            SPAT Devices by Music Unit: Spatial Sound at the Cutting Edge
+          </h1>
+          <p className="main1-photo-link">Download the Packs</p>
+        </a>
+      </article>
+    </section>
+  );
 }
 
 export default Main;
